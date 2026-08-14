19a5ffaec92e2e3b3fc3160aab9e1b8dcb351531
Vikram Gollapudi <gollapudi9621@students.d211.org>
2026-08-14 14:10:07 -0500

    Created first file HelloWorld

---
+++ M359/src/U1/notes/HelloWorld.java
@@ -0,0 +1,15 @@
+package U1.notes;
+
+public class HelloWorld {
+    // NEED THIS LINE SO WE CAN RUN CODE
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Is it Friday yet?");
+        int date = 13;
+        int month = 8;
+        double ratio =  1.0 * date/month;
+        System.out.println(ratio);
+        //Today is 8-13
+        System.out.println("Today is " + month + "-" + date);
+    }
+}
