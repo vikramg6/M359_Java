e0800b77cb769cc84cb3606451bd52940189edb4
Vikram Gollapudi <gollapudi9621@students.d211.org>
2026-08-14 14:41:27 -0500

    Created a funny face

---
+++ M359/src/U1/homework/FunnyFace.java
@@ -0,0 +1,21 @@
+package U1.homework;
+
+public class FunnyFace {
+    //pus shortcut PSVM to fill in the min method
+    public static void main(String[] args) {
+        //create a variable for your name and store your name
+        //create a variable for your age and store your age
+        String name = "Vikram";
+        int age = 14;
+        System.out.println(name + " is " + age + " years old.");
+        System.out.print("Happy");
+        System.out.println(" Friday");
+        //sout
+        System.out.println("I love sout");
+        /*
+        Use Severa; print line together with the tab \t
+        the new line \n and the quotation which is \"
+         */
+        System.out.println("------     ------ \n \n \n   --      --\n    --    --  \n       -- " );
+    }
+}
